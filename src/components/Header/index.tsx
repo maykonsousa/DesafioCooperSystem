d0c660d28df0a7de0c354bbfaa5f7f9a4d25f2c0
Maykon Eduardo Pereira De Sousa <mesousa1@latam.stefanini.com>
2021-03-20 05:53:20 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,13 @@ export const Header: React.FC = () => {
     <Container>
       <Content>
         <img src={logoImg} alt="GitShow" />
-        <button type="button">Sobre o Autor</button>
+        <a
+          href="http://maykonsousa.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button type="button">Sobre o Autor</button>
+        </a>
       </Content>
       <SearchBar />
     </Container>
